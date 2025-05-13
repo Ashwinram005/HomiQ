2602a9e38423c2c9e7882009fad559090c6a4731
Ashwinram005 <ashwinrammohan2005@gmail.com>
2025-05-13 10:54:18 +0530

    updated

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -25,4 +25,23 @@ const sendMessage = async (req, res) => {
   }
 };
 
-module.exports = { sendMessage };
+const getMessagesForChatRoom = async (req, res) => {
+  try {
+    const { chatRoomId } = req.params;
+
+    // Find all messages in the chat room
+    const messages = await Message.find({ chatRoom: chatRoomId })
+      .populate('sender', 'name email');  // This will populate the sender field with the name and email of the user
+
+    if (!messages.length) {
+      return res.status(404).json({ message: "No messages found" });
+    }
+
+    res.status(200).json(messages);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+module.exports = { sendMessage,getMessagesForChatRoom };
--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { sendMessage } = require("../controllers/messageController");
+const { sendMessage, getMessagesForChatRoom } = require("../controllers/messageController");
 
 router.post("/send", sendMessage);
-
+router.get("/:chatRoomId",getMessagesForChatRoom);
 module.exports = router;
