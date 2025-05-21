11def1c441967ac05e2e845b315719bb1b500a15
Ashwinram005 <ashwinrammohan2005@gmail.com>
2025-05-21 16:56:53 +0530

    updated cloudinary router

--- backend/routes/cloudinaryRoutes.js
+++ backend/routes/cloudinaryRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { getSignature } = require("../controllers/cloudinaryController");
 
-// Route to get signed signature for Cloudinary upload
 router.get("/sign", getSignature);
 
 module.exports = router;
